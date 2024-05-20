4f7955a6c55a7220e5b732c9bfa22e6fb40925c9
pihedron <pihedron@gmail.com>
2024-05-20 20:39:56 +0000

    added Enshadowed

--- Enshadowed/main.py
+++ Enshadowed/main.py
@@ -1,10 +1,38 @@
-sx, sy, ex, ey = map(int, input().split())
+import heapq
+
+sx, sy, ex, ey = map(float, input().split())
 
 N = int(input()) # number of shadows
 
-vertices = []
+vertices = [(sx, sy), (ex, ey)]
 
 for i in range(N):
-    x, y, r = map(int, input().split())
+    x, y, r = map(float, input().split())
     vertices.append((x, y))
 
+def dijkstra():
+    heap = [(0, 0)]
+    visited = set()
+    dist = [10 ** 9 for _ in range(N)]
+    dist[0] = 0
+    while heap:
+        (cost, u) = heapq.heappop(heap)
+        x, y = vertices[u]
+        if u in visited:
+            continue
+        visited.add(u)
+        if x == ex and y == ey:
+            break
+        for v in range(1, N):
+            if v in visited or v == u:
+                continue
+            dx = vertices[v][0] - x
+            dy = vertices[v][1] - y
+            w = (dx ** 2 + dy ** 2) ** 0.5
+            alt = cost + w
+            if dist[v] > alt:
+                dist[v] = alt
+                heapq.heappush(heap, (dist[v], v))
+    return dist[1]
+
+print(dijkstra())
