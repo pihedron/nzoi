de78e49bd29aaaf4c1f3ebd7a975ddad7d1f53c9
pihedron <pihedron@gmail.com>
2024-05-21 08:32:54 +0000

    fixed Enshadowed algorithm

--- Enshadowed/main.py
+++ Enshadowed/main.py
@@ -1,38 +1,42 @@
 import heapq
 
+DP = 6
+
 sx, sy, ex, ey = map(float, input().split())
 
 N = int(input()) # number of shadows
 
-vertices = [(sx, sy), (ex, ey)]
+vertices = [(sx, sy, 0), (ex, ey, 0)]
 
 for i in range(N):
     x, y, r = map(float, input().split())
-    vertices.append((x, y))
+    vertices.append((x, y, r))
+
+V = len(vertices)
 
 def dijkstra():
     heap = [(0, 0)]
     visited = set()
-    dist = [10 ** 9 for _ in range(N)]
+    dist = [10 ** 9 for _ in range(V)]
     dist[0] = 0
     while heap:
         (cost, u) = heapq.heappop(heap)
-        x, y = vertices[u]
+        x, y, r = vertices[u]
         if u in visited:
             continue
         visited.add(u)
         if x == ex and y == ey:
             break
-        for v in range(1, N):
+        for v in range(1, V):
             if v in visited or v == u:
                 continue
             dx = vertices[v][0] - x
             dy = vertices[v][1] - y
-            w = (dx ** 2 + dy ** 2) ** 0.5
+            w = max(0, (dx ** 2 + dy ** 2) ** 0.5 - r - vertices[v][2])
             alt = cost + w
             if dist[v] > alt:
                 dist[v] = alt
                 heapq.heappush(heap, (dist[v], v))
     return dist[1]
 
-print(dijkstra())
+print(round(dijkstra(), DP))
